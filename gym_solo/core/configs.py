2825560bc6978a32cfa6485628c306f437cd1c5a
Ankur Gupta <rukna1000@gmail.com>
2020-10-06 11:34:00 -0400

    Update start pos

--- gym_solo/core/configs.py
+++ gym_solo/core/configs.py
@@ -8,7 +8,7 @@ import pkg_resources
 class Solo8BaseConfig:
   dt: float = 1e-3
   motor_torque_limit: float = 1.5
-  robot_start_pos: Tuple[float] = (0., 0., 1.)
+  robot_start_pos: Tuple[float] = (0., 0., 0.)
   robot_start_orientation_euler: Tuple[float] = (0., 0., 0.)
   gravity: Tuple[float] = (0., 0., -9.81)
 
