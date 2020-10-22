5043833b90ff448450a45a23fb73631c80d420c8
Ankur Gupta <rukna1000@gmail.com>
2020-10-21 23:09:02 -0400

    Add the interactive demo

--- examples/solo8_vanilla_interactive.py
+++ examples/solo8_vanilla_interactive.py
@@ -1,5 +1,6 @@
 import argparse
 import gym
+import numpy as np
 
 import gym_solo
 from gym_solo.envs import solo8v2vanilla
@@ -8,3 +9,19 @@ from gym_solo.envs import solo8v2vanilla
 if __name__ == '__main__':
   config = solo8v2vanilla.Solo8VanillaConfig()
   env = gym.make('solo8vanilla-v0', use_gui=True, realtime=True, config=config)
+
+  try:
+    print("""\n
+          =========================
+              Solo 8 v2 Vanilla
+              
+          Simulation Active.
+          
+          Exit with ^C.
+          =========================
+          """)
+
+    while True:
+      env.step(np.zeros(env.action_space.shape))
+  except KeyboardInterrupt:
+    pass
