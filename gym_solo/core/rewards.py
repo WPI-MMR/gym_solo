0d3b856ac531cb5cc8d5ff50542c1863ded8d7c1
Ankur Gupta <rukna1000@gmail.com>
2020-10-27 17:12:09 -0400

    Add tests + rewards

--- gym_solo/core/rewards.py
+++ gym_solo/core/rewards.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import List
 
+from gym_solo import solo_types
+
 
 class Reward(ABC):  
   @abstractmethod
@@ -15,17 +17,47 @@ class Reward(ABC):
 
 
 @dataclass
-class WeightedReward:
+class _WeightedReward:
   reward: Reward
   weight: float
 
 
 class RewardFactory:
+  """A factory to dynamically create rewards.
+  
+  Note that this factory is currently implemented to combined rewards via
+  a linear combination. For example, if the user wanted to register rewards
+  r1, r2, and r3, the final reward would be r1 + r2 + r3. 
+  
+  Obviously, you can add coefficients to the rewards, and that functionality 
+  is further explained in register_reward() below. 
+  
+  If you need more functionality then a linear combination (exponential 
+  temporal decay), then it's probably in your best interest to implement that
+  in a custom Reward.
+  """
   def __init__(self):
-    self._rewards: List[WeightedReward] = []
+    """Create a new RewardFactory."""
+    self._rewards: List[_WeightedReward] = []
 
-  def register_reward(self, reward, weight):
-    self._rewards.append(reward=reward, weight=weight)
+  def register_reward(self, weight: float, reward: Reward):
+    """Register a reward to be computed per state.
 
-  def get_reward(self):
+    Args:
+      weight (float): The weight to be applied to this reward when it is 
+        is combined linearly with the other rewards. The domain for this
+        value is (-∞, ∞).
+      reward (Reward): A Reward object which .compute() will be called on at
+        reward computation time.
+    """
+    self._rewards.append(_WeightedReward(reward=reward, weight=weight))
+
+  def get_reward(self) -> float:
+    """Evaluate the current state and get the combined reward.
+
+    Returns:
+      float: The reward from the current state. Note that this reward is a 
+      combination of multiple atomic sub-rewards, as explained by the 
+      strategies earlier.
+    """
     return sum(wr.weight * wr.reward.compute() for wr in self._rewards)
