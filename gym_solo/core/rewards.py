f4c77454a08c4001e461678db991f49ae612f274
Ankur Gupta <rukna1000@gmail.com>
2020-10-27 15:40:33 -0400

    Add emtpy test file for reward factory

--- gym_solo/core/rewards.py
+++ gym_solo/core/rewards.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import List
+
 
 class Reward(ABC):  
   @abstractmethod
@@ -6,6 +9,23 @@ class Reward(ABC):
     """Compute the reward for the current state.
 
     Returns:
-        solo_types.reward: The reward evalulated at the current state.
+      solo_types.reward: The reward evalulated at the current state.
     """
     pass
+
+
+@dataclass
+class WeightedReward:
+  reward: Reward
+  weight: float
+
+
+class RewardFactory:
+  def __init__(self):
+    self._rewards: List[WeightedReward] = []
+
+  def register_reward(self, reward, weight):
+    self._rewards.append(reward=reward, weight=weight)
+
+  def get_reward(self):
+    return sum(wr.weight * wr.reward.compute() for wr in self._rewards)
---
+++ gym_solo/core/test_rewards_factory.py
@@ -0,0 +1,10 @@
+import unittest
+from gym_solo.core import rewards
+
+
+class TestRewardFactory(unittest.TestCase):
+  pass
+
+
+if __name__ == '__main__':
+  unittest.main()
