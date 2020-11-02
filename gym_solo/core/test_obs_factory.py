5b12c56e65560f16d1959576ad9b018b9804e9af
Ankur Gupta <rukna1000@gmail.com>
2020-11-02 15:12:37 -0500

    Refactor compliantobs

--- gym_solo/core/test_obs_factory.py
+++ gym_solo/core/test_obs_factory.py
@@ -1,20 +1,9 @@
 import unittest
 from gym_solo.core import obs
+from gym_solo.testing import CompliantObs
 
 from gym import spaces
 import numpy as np
-
-
-class CompliantObs(obs.Observation):
-  observation_space = spaces.Box(low=np.array([0., 0.]), 
-                                 high=np.array([3., 3.]))
-  labels = ['1', '2']
-
-  def __init__(self, body_id):
-    pass
-
-  def compute(self):
-    return np.array([1, 2])
 
 
 class TestObservationFactory(unittest.TestCase):
--- gym_solo/testing.py
+++ gym_solo/testing.py
@@ -1,6 +1,22 @@
+from gym_solo.core import obs
 from gym_solo.core import rewards
+
+from gym import spaces
+import numpy as np
 
 
 class SimpleReward(rewards.Reward):
   def compute(self):
     return 1
+
+
+class CompliantObs(obs.Observation):
+  observation_space = spaces.Box(low=np.array([0., 0.]), 
+                                 high=np.array([3., 3.]))
+  labels = ['1', '2']
+
+  def __init__(self, body_id):
+    pass
+
+  def compute(self):
+    return np.array([1, 2])
