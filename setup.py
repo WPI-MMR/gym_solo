04e68d55f0ebd2e942b126a708b4f98b292485d9
Ankur Gupta <rukna1000@gmail.com>
2021-02-05 13:38:10 -0500

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(name='gym_solo',
       version='0.0.1',
+      packages=find_packages(),
       install_requires=['gym', 'pybullet'],
       extras_require={
         'test': ['parameterized', 'pyvirtualdisplay', 'xvfbwrapper']
