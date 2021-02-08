bbe6b2ba5a443bbcd20f40877128f912ad8ca732
Ankur Gupta <rukna1000@gmail.com>
2021-02-08 18:04:15 -0500

    All tests are passing and done

--- gym_solo/core/test_obs_factory.py
+++ gym_solo/core/test_obs_factory.py
@@ -98,6 +98,25 @@ class TestObservationFactory(unittest.TestCase):
                                   np.array([1, 2, 5, 6]))
     self.assertListEqual(labels, ['1', '2', '5', '6'])
 
+  def test_get_obs_normalized(self):
+    of = obs.ObservationFactory(self.client, normalize=True)
+
+    test_obs1 = CompliantObs(None)
+    test_obs1.compute = lambda: np.array([1.5, 1.5])
+    of.register_observation(test_obs1)
+
+    test_obs2 = CompliantObs(None)
+    test_obs2.compute = lambda: np.array([0., 0.])
+    of.register_observation(test_obs2)
+
+    test_obs3 = CompliantObs(None)
+    test_obs3.compute = lambda: np.array([3., 3.])
+    of.register_observation(test_obs3)
+
+    observations, _ = of.get_obs()
+    np.testing.assert_array_equal(observations, 
+                                  np.array([0., 0., -1., -1., 1., 1.]))
+
   def test_get_observation_space_no_observations(self):
     of = obs.ObservationFactory(self.client)
     with self.assertRaises(ValueError):
